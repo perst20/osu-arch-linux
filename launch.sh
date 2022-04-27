aa34a65850f3d714a1404d361cc0dc042d7e4dbc
Ilya Perstenev <perst20@gmail.com>
2022-04-27 04:53:16 +0300

    log

--- launch.sh
+++ launch.sh
@@ -7,4 +7,4 @@ export WINEPREFIX="$SCRIPT_PATH/prefix"
 export PATH="/opt/wine-osu/bin:${PATH}"
 cd "./prefix/drive_c/users/$USER/osu"
 
-vblank_sync=0 gamemoderun wine osu\!.exe "$@" > log.txt
+vblank_sync=0 gamemoderun wine osu\!.exe "$@" > "$SCRIPT_PATH/log.txt"
